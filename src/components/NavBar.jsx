9e2e5836b7b8b0b099b14964fef4e9720af87107
nikola.cvetkovic <cvelekraljevo@gmail.com>
2019-05-31 16:16:30 +0200

    converted navbar to function

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,24 @@
 import '../styles/NavBar.css';
 
-import React, { Component } from 'react';
+import React from 'react';
 
 import refreshLogo from '../assets/refresh-button.png';
 
-class NavBar extends Component {
-  state = {};
-  render() {
-    return (
-      <React.Fragment>
-        <nav className="navbar navbar-light nav-bar-custom">
-          <a className="navbar-brand m-2 nav-bar-content" href="/">
-            <b> HackerNews</b>
-          </a>
-          <button
-            className="btn btn-default btn-color btn-lg ml-3 mb-4"
-            onClick={e => this.props.refreshPage(e)}
-          >
-            <img src={refreshLogo} alt={refreshLogo} width="20px" />
-          </button>
-        </nav>
-      </React.Fragment>
-    );
-  }
+function NavBar(props) {
+  return (
+    <React.Fragment>
+      <nav className="navbar navbar-light nav-bar-custom">
+        <a className="navbar-brand m-2 nav-bar-content" href="/">
+          <b>HackerNews</b>
+        </a>
+        <button
+          className="btn btn-default btn-color btn-lg ml-3 mb-4"
+          onClick={e => props.refreshPage(e)}
+        >
+          <img src={refreshLogo} alt={refreshLogo} width="20px" />
+        </button>
+      </nav>
+    </React.Fragment>
+  );
 }
-
 export default NavBar;
