cd146718b60d7719891e831767870fb7e8c83b18
nikola.cvetkovic <cvelekraljevo@gmail.com>
2019-05-31 14:12:00 +0200

    link to comments on hackernews

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -11,6 +11,8 @@ class NewsItem extends Component {
   render() {
     let hoursA = moment(this.state.item.time).hours();
     let commentsNum = this.state.item.kids ? this.state.item.kids.length : null;
+    let commentsLink =
+      "https://news.ycombinator.com/item?id=" + this.state.item.id;
     return (
       <li className="list-group-item">
         <p>
@@ -21,8 +23,14 @@ class NewsItem extends Component {
           {this.state.item.score} <span> points</span>
           <span className="gray-text"> by </span>
           {this.state.item.by}{" "}
-          <span className="gray-text">{hoursA} hours ago</span> |{" "}
-          {commentsNum ? commentsNum + " comments" : "no comments"}
+          <span className="gray-text">{hoursA} hours ago</span> |
+          {commentsNum ? (
+            <a target="_blank" href={commentsLink}>
+              {commentsNum} comments
+            </a>
+          ) : (
+            "no comments"
+          )}
         </p>
       </li>
     );
